13cccc93d86d7a7fc5074da17a3e1b114560e15a
Samiul Khan <toughlifesamiul@gmail.com>
2022-03-23 23:50:33 +0530

    dom project design done

--- 21class_DomProject/index.js
+++ 21class_DomProject/index.js
@@ -1,8 +1,20 @@
 console.log("hello world");
 
 const addBtn = document.querySelector(".add-btn")
+const modelCont = document.querySelector(".modal-cont");
 addBtn.addEventListener("click", function (e)
 {
-    console.log("clicked");
+    if (modelCont.style.display == 'flex')
+    {
+
+        
+
+
+        modelCont.style.display = 'none';   
+    }
+    else
+    {
+        modelCont.style.display = 'flex';    
+    }
     
 })
